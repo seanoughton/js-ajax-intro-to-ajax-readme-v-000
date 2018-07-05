0e4442b2ddd1b18669ce81b61997487d2a0ad631
Sean Oughton <seanoughton@gmail.com>
2018-07-05 18:32:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function showCommits() {
   //turns this into a json object
   const commits = JSON.parse(this.responseText)
 // creates the html by going through the commits collection and pulling out the data needed and putting it into a li
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.committer.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.committer.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   //gets the DOM element and puts the commitsList html into the div
   document.getElementById("commits").innerHTML = commitsList
 }
